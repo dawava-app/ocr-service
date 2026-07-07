0df11a1848abbeb51a86d2f3790f8de1fb5a39ed
unknown <medohelmy944@gmail.com>
2026-07-07 15:17:53 +0300

    Inicommit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-<<<<<<< HEAD
 
 from app.controllers.ocr_controller import router
 
@@ -7,14 +6,3 @@ app = FastAPI(title="Medicine OCR API")
 
 
 app.include_router(router, prefix="/ocr")
-=======
-from app.controllers import ocr_controller
-
-app = FastAPI()
-
-app.include_router(
-    ocr_controller.router,
-    prefix="/ocr",
-    tags=["OCR"]
-)
->>>>>>> 8cf4df6bd2031e4c282704330e2d754150a5e0c5
